833082975b62440f014fc74e0be6bb659798447a
BeranSantur <beransantur@gmail.com>
2020-05-25 00:42:47 +0300

    Revert "Update Introduction.java"

--- Introduction.java
+++ Introduction.java
@@ -21,9 +21,8 @@ class Main{
 public static void main (String[] args){
   LearnGitHub learnGitHub=new LearnGitHub("beran santur","learning github which seems a bit confusing");
   System.out.println("Name = "+learnGitHub.getName()+" purpose = "+learnGitHub.getPurpose());
-  System.out.println("Creating a new branch");
+
 }
- 
 
 
 
