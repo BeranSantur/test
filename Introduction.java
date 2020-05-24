0cfff1e1584e8f4c0e9012b4bc07868ee14a05d9
BeranSantur <beransantur@gmail.com>
2020-05-24 23:55:31 +0300

    Introduction has been created

---
+++ Introduction.java
@@ -0,0 +1,29 @@
+public class LearnGitHub
+{
+ private String name;
+ private String purpose;
+ 
+ public LearnGitHub(String name, String purpose){
+ this.name=name;
+ this.purpose=purpose;
+ }
+ 
+ public String getName(){
+ return this.name;
+ } 
+ public String getPurpose(){
+ return this.purpose;
+ }
+
+
+}
+class Main{
+public static void main(String[] args){
+  LearnGitHub learnGitHub=new LearnGitHub("beran santur","learning github which seems a bit confusing");
+  System.out.println("Name = "+learnGitHub.getName()+" purpose = "+learnGitHub.getPurpose());
+
+}
+
+
+
+}
